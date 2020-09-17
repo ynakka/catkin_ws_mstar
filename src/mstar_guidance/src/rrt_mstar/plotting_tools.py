d185fa2feef1b16df48fd447a45b57112b1c7219
Yashwanth kumar Nakka <32655645+ynakka@users.noreply.github.com>
2020-09-17 15:37:24 -0700

    Add files via upload

---
+++ src/mstar_guidance/src/rrt_mstar/plotting_tools.py
@@ -0,0 +1,42 @@
+
+import numpy as np
+import scipy as sp
+import matplotlib  
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+from matplotlib.patches import Circle
+from matplotlib.patches import Rectangle
+
+import matplotlib.transforms as transforms
+
+
+def rectagular_obstacle(left_corner_x,left_corner_y,width,height,ax,angle=0.0,facecolor='none',**kwargs):
+    addrectangle = Rectangle((left_corner_x,left_corner_y),width=width,height=height,angle=angle,facecolor=facecolor, **kwargs) 
+    return ax.add_patch(addrectangle)
+
+def circular_obstacle(x, y, radius, ax, facecolor='none', **kwargs):
+    
+    """
+    Create a plot of the obstacle.
+
+    Parameters
+    ----------
+    x, y : is the center of the obstacle
+        Input data.
+
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    Returns
+    -------
+    matplotlib.patches.circle
+
+    Other parameters
+    ----------------
+    kwargs : `~matplotlib.patches.Patch` properties
+    """
+    center_x = x
+    center_y = y 
+    add_circle = Circle((center_x,center_y),radius,facecolor=facecolor, **kwargs) 
+    return ax.add_patch(add_circle)
+
--- src/mstar_guidance/src/rrt_mstar/rrt_ao_mstar.py
+++ src/mstar_guidance/src/rrt_mstar/rrt_ao_mstar.py
@@ -3,7 +3,10 @@
 import numpy as np
 import math as mt  
 import random
+import hnswlib
 
+import matplotlib.pyplot as plt
+import csv
 """
 ao - rrt ??
 no optimization 
@@ -11,7 +14,7 @@ control sampling and propagation
 
 """
 
-def f_dyn(x,control,mass,inertia,moment_arm):
+def mstar_dyn(x,control,mass,inertia,moment_arm):
     x = x.reshape(6)
     control = control.reshape((8,1))
 
@@ -32,7 +35,7 @@ def f_dyn(x,control,mass,inertia,moment_arm):
 
 
 def euler_step(state_old,control,dt,mass,inertia,moment_arm):
-    state_new = state_old + f_dyn(state_old,control,mass,inertia,moment_arm)*dt
+    state_new = state_old + mstar_dyn(state_old,control,mass,inertia,moment_arm)*dt
     return state_new
 
 def distance_to_goal(node,goal_state):
@@ -43,18 +46,18 @@ def distance_to_goal(node,goal_state):
 
 def check_state_bounds(node,state_min,state_max):
     if (node < state_min).any() or (node > state_max).any():
-        return False
-    else:
         return True
+    else:
+        return False
 
 def collision_checking(node,obstacle_list,num_obstacles):    
     for i in range(num_obstacles):
         pos = node[0:2]
         obstaclepos = obstacle_list[i][0][0:2]
         if np.linalg.norm(pos-obstaclepos) <= obstacle_list[i][1]:
-            return True 
+            return True
+    return False
 
-    return False 
 
 def sample_state(state_min,state_max):
     node_sample = np.random.uniform(state_min,state_max)    
@@ -64,15 +67,172 @@ def sample_control(num_control,Umin,Umax):
     control_sample = np.random.uniform(Umin,Umax,num_control)
     return control_sample
 
-
 def rrt_ao_mstar(state_init,state_goal,dt,\
     obstacle_list,state_min,state_max,Umin,Umax,mass,inertia,moment_arm,\
-    propagation_steps = 2,iters = 100000,cost_max = 1e6):
+    propagation_steps = 4,iters = 100000,cost_limit = 1e6,trials=10,top_k=100):
 
+    """
+    This is taken from Wolfgangs Code.
 
-    return xsol, usol, tvec 
+    """
+    sample_goal_iter = 50
+    parents = -np.ones((iters,),dtype=np.int)
+    states = np.zeros((iters,6),dtype=np.float32)
+    states_temp = np.zeros((iters,propagation_steps+1,6),dtype=np.float32) # includes the parent node
+    actions = np.zeros((iters,8),dtype=np.float32)
+    timesteps = np.zeros((iters,),dtype=np.int)
+    cost = np.zeros((iters,),dtype=np.float32)
     
+    expand_attempts = np.zeros((iters,),dtype=np.int)
+    expand_successes = np.zeros((iters,),dtype=np.int)
+
+    num_obstacles = len(obstacle_list)
+    # begin trial
+    
+    for trail in range(trials):
+        states[0] = state_init
+        i = 1
+        attempt = 0
+        best_distance = 1e6
+        best_i = 0
+        best_cost = cost_limit
+
+        index = hnswlib.Index(space='l2', dim=6)
+        # ef_construction - controls index search speed/build speed tradeoff
+        #
+        # M - is tightly connected with internal dimensionality of the data. Strongly affects memory consumption (~M)
+        # Higher M leads to higher accuracy/run_time at fixed ef/efConstruction
+        index.init_index(max_elements=iters, ef_construction=100, M=16)
+
+        # Controlling the recall by setting ef:
+        # higher ef leads to better accuracy, but slower search
+        index.set_ef(100)
+        index.add_items(states[0])
+
+        sol_x = None
+        sol_u = None
+
+        while attempt < iters:
+            attempt +=1
+            print(attempt)
+            # randomly sample a state
+            if attempt % sample_goal_iter == 0:
+                idx = best_i
+                # x = state_goal
+            else:
+                x = sample_state(state_min,state_max)
+                ids, distances = index.knn_query(x, k=min(top_k,i))
+                # idx = ids[0,np.random.randint(0, ids.shape[1])]
+                idx = ids[0,0]
+            
+            u = sample_control(8,Umin,Umax)
+            cost[i] = cost[idx] + np.linalg.norm(u)
+            if cost[i] > cost_limit:
+                continue
+
+            timesteps[i] = timesteps[idx] + propagation_steps
+
+            # forward propagate
+            states_temp[i,0] = states[idx]
+            all_states_valid = True
+
+            for k in range(1,propagation_steps+1):
+            # propagate
+                # states_temp[i,k] = robot.step(torch.from_numpy(states_temp[i,k-1]), torch.from_numpy(u), data_neighbors_next_i, dt).detach().numpy()
+                states_temp[i,k] = euler_step(states_temp[i,k-1],u,dt,mass,inertia,moment_arm)
+            # validity check
+                if check_state_bounds(states_temp[i,k],state_min,state_max) or collision_checking(states_temp[i,k],obstacle_list,num_obstacles):
+                # if not state_valid(robot, states_temp[i,k], data_neighbors_next_i) or not:
+                    all_states_valid = False
+                    # print('all states valid:',all_states_valid)
+                    break
+            
+            if not all_states_valid:
+                continue
+
+            # update data structures
+            parents[i] = idx
+            states[i] = states_temp[i,-1]
+            actions[i] = u
+
+            expand_successes[idx] += 1
+
+            index.add_items(states[i:i+1])
+
+            # dist = np.linalg.norm(states[i,0:3] - xf[0:3])
+            dist = np.linalg.norm(states[i,0:2] - state_goal[0:2])
+
+            # find best solution to goal
+            if best_distance > dist:
+                best_distance = dist
+            #   print(dist)
+                best_i = i
+            # print("best distance: ", best_distance, best_i, attempt)
+
+            i+=1
+            # print('increased the step')
+            # print('best distance :', best_distance)
+            if best_distance < 0.1:
+                print('reached goal')
+                idx = best_i
+                best_cost = cost[idx]
+                cost_limit = 0.9 * cost[idx]
+
+                sol_x = []
+                sol_u = []
+                while idx > 0:
+                    for k in reversed(range(1, propagation_steps+1)):
+                        sol_x.append(states_temp[idx,k])
+                        sol_u.append(actions[idx])
+                        idx = parents[idx]
+                sol_x.append(states[0])
+                sol_x.reverse()
+                sol_u.reverse()
+
+                sol_npx = np.array(sol_x)
+                sol_npu = np.array(sol_u)
+
+                return sol_npx, sol_npu
+
+    # return sol_x, sol_u
+    
+if __name__ == "__main__":
+    state_init = np.zeros(6)
+    state_goal = np.array([1,1,0,0,0,0])
+
+    dt = 0.5
+    state_min = np.array([0,0,-mt.pi,-1,-1,-10])
+    state_max = np.array([1,1,mt.pi,1,1,10])
+
+    Umin = 0
+    Umax = 0.45
+
+    mass = 10 
+    inertia = 1.6
+    moment_arm = 0.2
+
+    obstacle_list = []
+    # obstacle state and distance between the robot and the obstacle
+    obstacle1 = [np.array([0.5,0.5,0,0,0,0]),0.2]
+    obstacle_list.append(obstacle1)
+    obstacle2 = [np.array([0.7,0.3,0,0,0,0]),0.2]
+    obstacle_list.append(obstacle2)
+    # obstacle3 = [np.array([3,3,0,0,0,0]),1]
+    # obstacle_list.append(obstacle3)
+
+    solx, solu = rrt_ao_mstar(state_init,state_goal,dt,\
+    obstacle_list,state_min,state_max,Umin,Umax,mass,inertia,moment_arm,\
+    propagation_steps = 2,iters = 100000,cost_limit = 1e6,trials=10,top_k=100)
+
+    # print(len(solx))
+    # print(len(solu))
+
+    # print(np.shape(np.array(solx)))
+
+    # print(np.shape(np.array(solu)))
+
+    # print(solx)
+    # print(np.array(solx))
 
 
-
-
+        
